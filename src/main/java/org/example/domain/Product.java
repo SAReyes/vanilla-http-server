b699992dc587b95a8f3ff6cec198858f7faeb437
Adrian Reyes <sadrian.reyes@gmail.com>
2018-12-08 17:56:31 +0100

    ADD domain objects

---
+++ src/main/java/org/example/domain/Product.java
@@ -0,0 +1,42 @@
+package org.example.domain;
+
+import java.util.List;
+
+public class Product {
+    private long id;
+    private String name;
+    private List<Department> departments;
+    private List<Category> categories;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Department> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<Department> departments) {
+        this.departments = departments;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+}
